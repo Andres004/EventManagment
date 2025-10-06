c6a7ecd216088435baec2fea285c59e93c720fbe
Josue-Camacho <josuecamacho582@gmail.com>
2025-10-06 07:36:11 -0400

    Fix: program

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using EventManagment.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace EventManagment.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Ticket> Tickets => Set<Ticket>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Ticket>(t =>
+            {
+                t.HasKey(x => x.Id);
+
+                // Configuración para almacenar List<string> en la base de datos
+                t.Property(x => x.Notes)
+                    .HasConversion(
+                        v => v == null ? null : string.Join(';', v),
+                        v => string.IsNullOrEmpty(v) ? new List<string>() : v.Split(';', StringSplitOptions.RemoveEmptyEntries).ToList()
+                    )
+                    .HasMaxLength(500); // Longitud máxima para el campo en la BD
+            });
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,23 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using EventManagment.Data;
+using EventManagment.Repositories;
+using EventManagment.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<AppDbContext>(opt =>
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<ITicketRepository, TicketRepository>();
+builder.Services.AddScoped<ITicketService, TicketService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
