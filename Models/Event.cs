10d2377dbb0af2350b786b86d1a04b093d69a7c9
Adrián Mercado <you@example.com>
2025-10-06 12:38:05 -0400

    ft: created model Event

--- Models/Event.cs
+++ Models/Event.cs
@@ -2,5 +2,9 @@
 {
     public class Event
     {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public DateTime Date { get; set; }
+        public int Capacity { get; set; }
     }
 }
