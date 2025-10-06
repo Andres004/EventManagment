0d12520b9d954c7b6151be8d6c6d89c3bd7cbbfd
Adrián Mercado <you@example.com>
2025-10-06 12:53:11 -0400

    ft: added IEventRepository

---
+++ Repositories/IEventRepository.cs
@@ -0,0 +1,13 @@
+﻿using EventManagment.Models;
+
+namespace EventManagment.Repositories
+{
+    public interface IEventRepository
+    {
+        Task<List<Event>> GetAllAsync();
+        Task<Event?> GetByIdAsync(Guid id);
+        Task AddAsync(Event e);
+        Task UpdateAsync(Event e);
+        Task DeleteAsync(Guid id);
+    }
+}
